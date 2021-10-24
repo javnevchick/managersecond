4a920729ab7a38a022c39a58d67b62f46b4bc972
user <radiy542@gmail.com>
2021-10-24 19:13:45 +0300

    Осталось валидацию

--- src/main/java/com/fakhritdinov/managersecond/controller/FirstpageController.java
+++ src/main/java/com/fakhritdinov/managersecond/controller/FirstpageController.java
@@ -1,16 +1,22 @@
 package com.fakhritdinov.managersecond.controller;
 
 import com.fakhritdinov.managersecond.model.Client;
+import com.fakhritdinov.managersecond.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @Controller
 public class FirstpageController {
+
+    private final ClientService clientService;
+
+    @Autowired
+    public FirstpageController(ClientService clientService) {
+        this.clientService = clientService;
+    }
 
     @GetMapping
     public String greeting() {
@@ -18,8 +24,22 @@ public class FirstpageController {
     }
 
     @GetMapping("/search-client")
-    public String searchClient(Client client, Model model) {
-        model.addAttribute("client", client);
+    public String searchClient(
+            @RequestParam(value = "contacte") String contacte,
+            @RequestParam(value = "fielde") String fielde,
+            Model model)
+    {
+        if(contacte.equals("fio")) {
+            Client client = clientService.findByName(fielde);
+            model.addAttribute("client", client);
+        } else if(contacte.equals("passport")) {
+            Client client = clientService.findByPassport(fielde);
+            model.addAttribute("client", client);
+        } else if (contacte.equals("contactNumber")) {
+            Client client = clientService.findByContactNumber(fielde);
+            model.addAttribute("client", client);
+        }
+
         return "index";
     }
 
